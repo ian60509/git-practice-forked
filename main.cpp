01b873ea882fd62bdbee8b95d2c966b5108eb2d5
ian60509 <ian.60509@gmail.com>
2024-01-09 16:46:47 +0800

    got sick again

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 
 int main(){
     int a = 5;
+    printf("hello world \n")
+    printf("I got sick, ohhhhh")
+    printf("I got sick again")
     printf("hello world ,hahaha\n")
     printf("I got sick")
 }
